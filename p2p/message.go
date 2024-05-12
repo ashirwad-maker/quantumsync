d9df33bad232b41154705652167bb7bb0bbb4d46
ashirwad-maker <ashirwadsinha@gmail.com>
2024-05-12 21:00:24 +0530

    Added RPC

--- p2p/message.go
+++ p2p/message.go
@@ -1,8 +1,12 @@
 package p2p
 
-// Message represents any abriitary data that is being sent over the each transport
-// between two nodes in the network.
-type Message struct {
+import "net"
+
+// RPC (Remote Procedure Call) represents any abriitary data that is being sent
+// over the each transport between two nodes in the network.
+// RPC in context of p2p(peer to peer) can invoke procedures or functions on remote
+// peers allowing a computer program to execute on a remote node.
+type RPC struct {
 	Payload []byte
-	From    string
+	From    net.Addr
 }
