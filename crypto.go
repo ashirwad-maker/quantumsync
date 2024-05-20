f13a92e08780b09d039bb5ca49853f32014f8441
ashirwad-maker <ashirwadsinha@gmail.com>
2024-05-20 20:21:11 +0530

    Refactoring

--- crypto.go
+++ crypto.go
@@ -13,30 +13,11 @@ func newEncryptionKey() []byte {
 	return keyBuf
 }
 
-// The function encrypts the data from an io.Reader and write the encrypted data on the io.Writer
-// using the AES encryption algorithm in CTR mode.
-func copyEncrypt(key []byte, src io.Reader, dst io.Writer) (int, error) {
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		return 0, err
-	}
-
-	iv := make([]byte, block.BlockSize())
-
-	//Create a buffer for the IV of size equal to the block size(16 bytes)
-	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
-		return 0, err
-	}
-	// Prepend the iv file
-	if _, err := dst.Write(iv); err != nil {
-		return 0, err
-	}
+func copyStream(stream cipher.Stream, blockSize int, src io.Reader, dst io.Writer) (int, error) {
 
 	// Making the buffer to th specific size so that io.Copy can work properly.
 	buf := make([]byte, 32*1024)
-	stream := cipher.NewCTR(block, iv) // Creates a CTR mode stream cipher with the AES block and IV
-	nw := block.BlockSize()
-
+	nw := blockSize
 	for {
 		n, err := src.Read(buf)
 		if n > 0 {
@@ -57,6 +38,31 @@ func copyEncrypt(key []byte, src io.Reader, dst io.Writer) (int, error) {
 	return nw, nil
 }
 
+// The function encrypts the data from an io.Reader and write the encrypted data on the io.Writer
+// using the AES encryption algorithm in CTR mode.
+func copyEncrypt(key []byte, src io.Reader, dst io.Writer) (int, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return 0, err
+	}
+
+	iv := make([]byte, block.BlockSize())
+
+	//Create a buffer for the IV of size equal to the block size(16 bytes)
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		return 0, err
+	}
+	// Prepend the iv file
+	if _, err := dst.Write(iv); err != nil {
+		return 0, err
+	}
+
+	stream := cipher.NewCTR(block, iv) // Creates a CTR mode stream cipher with the AES block and IV
+
+	return copyStream(stream, block.BlockSize(), src, dst)
+}
+
+// This function decrypts the data based on the key, which is unique to each server and is stored in EncKey
 func copyDecrypt(key []byte, src io.Reader, dst io.Writer) (int, error) {
 	block, err := aes.NewCipher(key)
 	if err != nil {
@@ -68,28 +74,9 @@ func copyDecrypt(key []byte, src io.Reader, dst io.Writer) (int, error) {
 	if _, err := src.Read(iv); err != nil {
 		return 0, err
 	}
+
 	// fmt.Println(iv)
-
-	buf := make([]byte, 32*1024)
 	stream := cipher.NewCTR(block, iv)
-	nw := block.BlockSize()
 
-	for {
-		n, err := src.Read(buf)
-		if n > 0 {
-			stream.XORKeyStream(buf, buf[:n])
-			nn, err := dst.Write(buf[:n])
-			if err != nil {
-				return 0, err
-			}
-			nw += nn
-		}
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			return 0, err
-		}
-	}
-	return nw, err
+	return copyStream(stream, block.BlockSize(), src, dst)
 }
